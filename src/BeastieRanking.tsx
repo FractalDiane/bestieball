acb2399a55d3c22f0017f20d9701638776217a4b
Diane Sparks <duncan.sprks@gmail.com>
2024-12-14 20:29:49 -0800

    prettyify results

--- src/BeastieRanking.tsx
+++ src/BeastieRanking.tsx
@@ -9,9 +9,13 @@ export default function BeastieRanking(props: BeastieRankingProps) {
 	return (<>
 		<div className="beastieRanking">
 			<div className="beastieRankingNumber">{props.rank}</div>
-			<img className="beastieIcon ranking" src={`https://beastiepediaimg.s3.amazonaws.com/d76cpgnhqk1hcr/${props.beastie.beastieName}Idle.png`} />
-			<div className="beastieName ranking">{props.beastie.beastieName}</div>
-			<div className="beastieNumber ranking"><span className="hash"><b>#</b></span>{props.beastie.beastieNumber.toString().padStart(2, "0")}</div>
+			<div className="beastieRankingIcon">
+				<img className="beastieIcon ranking" src={`https://beastiepediaimg.s3.amazonaws.com/d76cpgnhqk1hcr/${props.beastie.beastieName}Idle.png`} />
+			</div>
+			<div className="beastieRankingName">
+				<div className="beastieName ranking">{props.beastie.beastieName}</div>
+				<div className="beastieNumber ranking"><span className="hash"><b>#</b></span>{props.beastie.beastieNumber.toString().padStart(2, "0")}</div>
+			</div>
 		</div>
 	</>);
 }
