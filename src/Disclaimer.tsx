759772ff6accb38854017d05128ce0ffb40baf79
Diane Sparks <duncan.sprks@gmail.com>
2024-12-21 08:51:50 -0800

    metadata

--- src/Disclaimer.tsx
+++ src/Disclaimer.tsx
@@ -1,6 +1,6 @@
 export default function Disclaimer() {
 	return <div id="footer"><center>
-		<p>This is a for-fun fan project; <i>Beastieball</i> and all Beasties belong to Wishes Unlimited.</p>
+		<p>This is a for-fun fan project; <cite>Beastieball</cite> and all Beasties belong to Wishes Unlimited.</p>
 		<p><a href="https://store.steampowered.com/app/1864950/Beastieball/" target="_blank" rel="noopener noreferrer">Go buy the game here!</a></p>
 	</center></div>
 }
